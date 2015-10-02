3345579b7ff851b5b5f518438c034af6389e597f
James Trinklein <james.trinklein@changepoint.com>
2015-10-02 10:38:55 -0700

    add rabbit mq configuration for docker-compose

--- app/app.js
+++ app/app.js
@@ -11,13 +11,16 @@ var express = require('express'),
     util = require('util');
 
 function connectMQ(url) {
+    console.log('connecting to rabbitmq');
     return Q.ninvoke(amqp, 'connect', url)
 }
 function createMQChannel(connection) {
+    console.log('creating rabbitmq channel');
     return Q.ninvoke(connection, 'createChannel');
 }
 function handleTimesheetsMessage(message) {
     var msg = JSON.parse(message);
+    console.log('handling timesheets message: ' + msg.type);
     if(msg.type === 'ADD_ITEM') {
         var userId = msg.userId;
         var item = msg.item;
@@ -25,16 +28,30 @@ function handleTimesheetsMessage(message) {
     }
 }
 var mqChannel;
-connectMQ('amqp://rabbitmq')
+connectMQ('amqp://guest:guest@rabbit')
+    .fail(function(err) {
+        console.log('rabbitmq error: ' + err);
+    })
     .then(createMQChannel)
+    .fail(function(err) {
+        console.log('rabbitmq error: ' + err);
+    })
     .then(function(ch){
+        console.log('verify/create rabbitmq "timesheets" exchange');
         mqChannel = ch;
         ch.assertExchange('timesheets', 'fanout', {durable: false});
         return Q.ninvoke(ch, 'assertQueue', '', {exclusive: true});
     })
+    .fail(function(err) {
+        console.log('rabbitmq error: ' + err);
+    })
     .then(function(queue) {
+        console.log('register timesheets message handler');
         mqChannel.bindQueue(queue.queue, 'timesheets', '');
         mqChannel.consume(queue.queue, handleTimesheetsMessage, {noAck: true});
+    })
+    .fail(function(err) {
+        console.log('rabbitmq error: ' + err);
     });
 var mongoUrl = 'mongodb://' + (process.env.MONGO_PORT_27017_TCP_ADDR || 'localhost') + ':' + (process.env.MONGO_PORT_27017_TCP_PORT || '27017') + '/timesheets';
 
